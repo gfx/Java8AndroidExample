a207a31eecde4aecea5206e05a71d3ed7a19d740
FUJI Goro (gfx) <gfuji@cpan.org>
2017-05-07 23:50:35 +0900

    add use of retropiler

--- app/src/main/java/io/github/gfx/java8androidexample/MainActivity.java
+++ app/src/main/java/io/github/gfx/java8androidexample/MainActivity.java
@@ -1,13 +1,22 @@
 package io.github.gfx.java8androidexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import java.util.Optional;
+
+@SuppressLint("NewApi")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+
+        Optional.of("foo").map(String::toUpperCase).ifPresent(textView::setText);
     }
 }
